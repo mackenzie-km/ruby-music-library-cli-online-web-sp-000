36f442f6c846a78f6f30fd7e0852839eb8b3dd57
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-12 22:07:05 +0000

    Done.

--- lib/Genre.rb
+++ lib/Genre.rb
@@ -1,49 +1,49 @@
 require_relative "concerns/findable.rb"
 
-class Genre 
+class Genre
   extend Concerns::Findable
-  attr_accessor :name, :songs 
-  @@all = [] 
+  attr_accessor :name, :songs
+  @@all = []
 
   def initialize(name = nil)
-    if name !=nil 
-      @name = name 
+    if name !=nil
+      @name = name
       @songs = []
-    end 
-  end 
+    end
+  end
 
 def self.create(name)
   item = self.new(name)
   item.save
-  item 
-end 
+  item
+end
 
 def self.all
     @@all
-end 
+end
 
-def self.destroy_all 
-    @@all.clear 
-end 
+def self.destroy_all
+    @@all.clear
+end
 
 def name=(name)
-    @name = name 
-end 
+    @name = name
+end
 
-def save 
-    @@all << self 
-end 
+def save
+    @@all << self
+end
 
 def artists
-    artists = [] 
+    artists = []
     Song.all.each do |song|
-      if song.genre == self 
+      if song.genre == self
        artists << song.artist
        artists.uniq!
-      end 
-    end 
-      artists 
-  end 
+      end
+    end
+      artists
+  end
 
 
 end
